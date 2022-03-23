7ab808129c24bfb361a5c177aa69a18c26f10c7e
OctoM <orders@octom.ru>
2022-03-23 12:24:10 +0900

    Camera is done

--- front/src/pages/Camera/index.js
+++ front/src/pages/Camera/index.js
@@ -1,12 +1,24 @@
-import React from "react"
-import { SafeAreaView, View } from "react-native"
+import React from 'react'
+import {SafeAreaView, StyleSheet, TouchableOpacity, View} from 'react-native'
+import {RNCamera} from 'react-native-camera'
+import QRCodeScanner from 'react-native-qrcode-scanner'
+import Text from '../../components/Text'
 
 const Camera = () => {
-    return <SafeAreaView>
-        <View>
-            
-        </View>
-    </SafeAreaView>
+   const onSuccess = e => {
+      const {data} = e
+      let jsonData = {}
+      data.split('&').map(e => {
+         let object = e.split('=')
+         jsonData[object[0]] = object[1]
+      })
+      
+   }
+   return (
+      <SafeAreaView style={{justifyContent: 'center', alignItems: 'center', height: '100%', backgroundColor: '#1f1f1f'}}>
+         <QRCodeScanner onRead={onSuccess} />
+      </SafeAreaView>
+   )
 }
 
 export default Camera
